68cd4ee5229ebbff627ab9e34152fa5f61d84e36
Nathan Coulas <nathan@gadget.dev>
2023-08-31 14:38:38 -0400

    Install baseui & Styletron

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BaseProvider } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme } from "baseui";
+import { Client as Styletron } from "styletron-engine-atomic";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={new Styletron()}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>
 );
