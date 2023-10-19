8f7c6a2e9557e60bfc0ab4560caa7ca9ce512e5e
Nathan Coulas <contact.nathancoulas@gmail.com>
2023-10-18 20:07:44 -0400

    Setup react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,34 @@ import { BaseProvider } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { LightTheme } from "baseui";
 import { Client as Styletron } from "styletron-engine-atomic";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Apply } from "./components/Application/Apply.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+const Root = () => {
+  return (
     <StyletronProvider value={new Styletron()}>
       <BaseProvider theme={LightTheme}>
         <App />
       </BaseProvider>
     </StyletronProvider>
+  )
+}
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+    },
+    {
+      path: "/apply",
+      element: <Apply />,
+    },
+  ]
+)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
