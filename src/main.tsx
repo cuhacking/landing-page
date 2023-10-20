69099dd869d30d0105fa4d11e0bb4c6358c21977
Nathan Coulas <contact.nathancoulas@gmail.com>
2023-10-20 17:37:27 -0400

    Add providers to applications page (Sorry Isabella!)

--- src/main.tsx
+++ src/main.tsx
@@ -8,28 +8,28 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Apply } from "./components/Application/Apply.tsx";
 
-const Root = () => {
+const RootProviders = (props: { children: React.ReactNode }) => {
   return (
     <StyletronProvider value={new Styletron()}>
       <BaseProvider theme={LightTheme}>
-        <App />
+        {props.children}
       </BaseProvider>
     </StyletronProvider>
-  )
+  );
 }
 
 const router = createBrowserRouter(
   [
     {
       path: "/",
-      element: <Root />,
+      element: <RootProviders><App /></RootProviders>,
     },
     {
       path: "/apply",
-      element: <Apply />,
+      element: <RootProviders><Apply /></RootProviders>,
     },
   ]
-)
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
