e96e818cc46ddff6c3e68c8f4706dd344ac71816
Nathan Coulas <contact.nathancoulas@gmail.com>
2024-06-14 12:32:50 -0400

    Added MLH code of conduct

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -2,18 +2,17 @@ import "minireset.css";
 import "./HomePage.css";
 import { Hero } from "./heroSection/Hero";
 import { ConfettiProvider } from "./heroSection/Confetti";
-import { AboutSection } from "./aboutSection/AboutSection";
+// import { AboutSection } from "./aboutSection/AboutSection";
 import { SponsorsSection } from "./sponsorsSection/SponsorsSection";
 import { FaqSection } from "./faqSection";
 
 export const App = () => {
-
   return (
     <ConfettiProvider>
-        <Hero />
-        <AboutSection />
-        <FaqSection />
-        <SponsorsSection />
+      <Hero />
+      {/* <AboutSection /> */} {/* We are temporarily removing this section */}
+      <FaqSection />
+      <SponsorsSection />
     </ConfettiProvider>
   );
 };
--- src/pages/homePage/faqSection/faqAccordion/FaqAccordion.tsx
+++ src/pages/homePage/faqSection/faqAccordion/FaqAccordion.tsx
@@ -2,20 +2,30 @@ import { Faq } from "../types";
 import "./FaqAccordion.style.scss";
 
 interface FaqAccordionProps {
-    faq: Faq;
-    selected: boolean;
-    onClick: () => void;
+  faq: Faq;
+  selected: boolean;
+  onClick: () => void;
 }
 
-export const FaqAccordion = ({faq, selected, onClick }: FaqAccordionProps) => {
-    return (
-        <div className={`FaqAccordion ${selected ? "FaqAccordion--open" : "FaqAccordion--closed"}`} onClick={onClick}>
-            <div className="FaqAccordion__head">
-                {/* // TODO: should be svg */}
-                <button className="FaqAccordion__button">{selected ? "-" : "+"}</button>
-                <div className="FaqAccordion__question">{faq.question}</div>
-            </div>
-            {selected && (<div className="FaqAccordion__answer">{faq.answer}</div>)}
-        </div>
-    );
+export const FaqAccordion = ({ faq, selected, onClick }: FaqAccordionProps) => {
+  return (
+    <div
+      className={`FaqAccordion ${
+        selected ? "FaqAccordion--open" : "FaqAccordion--closed"
+      }`}
+      onClick={onClick}
+    >
+      <div className="FaqAccordion__head">
+        {/* // TODO: should be svg */}
+        <button className="FaqAccordion__button">{selected ? "-" : "+"}</button>
+        <div className="FaqAccordion__question">{faq.question}</div>
+      </div>
+      {selected && (
+        <div
+          dangerouslySetInnerHTML={{ __html: faq.answer }}
+          className="FaqAccordion__answer"
+        />
+      )}
+    </div>
+  );
 };
