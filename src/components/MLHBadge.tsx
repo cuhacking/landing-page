61cf9a61c7c2711626d857f7caf39ebfa51e6560
Mumtahin Farabi <mfarabi619@gmail.com>
2024-06-26 19:55:55 -0400

    refactor(Components/FrostedPanel): Convert SCSS file to Tailwind Classes

--- src/components/MLHBadge.tsx
+++ src/components/MLHBadge.tsx
@@ -4,7 +4,7 @@ export const MLHBadge = () => {
   return (
     <img
       src={mlh}
-      className="absolute top-0 right-[10vh] w-16 md:left-[8vh] md:w-20 lg:w-32 "
+      className="absolute top-0 right-[10vh] w-16 md:left-[8vh] md:w-20 lg:w-32"
       alt="MLH Badge"
     />
   );
